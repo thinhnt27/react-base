03bc33014861a043c436ee7a516dc7a2d70b333e
Trường Thịnh <thinhntse170224@fpt.edu.vn>
2024-04-09 10:13:31 +0700

    update

--- src/components/AddUserInfor.js
+++ src/components/AddUserInfor.js
@@ -1,54 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 
-class AddUserInfor extends React.Component {
-  state = {
-    name: "Thinh",
-    address: "Q9",
-    age: "20",
-  };
+// class AddUserInfor extends React.Component {
+//   state = {
+//     name: "Thinh",
+//     address: "Q9",
+//     age: "20",
+//   };
 
-  handleOnChangeInput = (event) => {
-    this.setState({
-      name: event.target.value,
-    });
-    // console.log(event, event.target.value);
+//   handleOnChangeInput = (event) => {
+//     this.setState({
+//       name: event.target.value,
+//     });
+//     // console.log(event, event.target.value);
+//   };
+//   handleOnChangeAge = (event) => {
+//     this.setState({
+//       age: event.target.value,
+//     });
+//     // console.log(event, event.target.value);
+//   };
+//   handleOnSubmit = (event) => {
+//     event.preventDefault();
+//     this.props.handleAddNewUser({
+//       id: Math.floor(Math.random() * 100 + 1) + "-random",
+//       name: this.state.name,
+//       age: this.state.age,
+//     });
+//   };
+//   render() {
+//     return (
+//       <div>
+//         My name is {this.state.name} I'm from {this.state.age}
+//         <form onSubmit={(event) => this.handleOnSubmit(event)}>
+//           <label>Your name:</label>
+//           <input
+//             type="text"
+//             value={this.state.name}
+//             onChange={(event) => this.handleOnChangeInput(event)}
+//           />
+//           <label>Your age:</label>
+//           <input
+//             type="text"
+//             value={this.state.age}
+//             onChange={(event) => this.handleOnChangeAge(event)}
+//           />
+//           <button>Submit</button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+
+const AddUserInfor = (props) => {
+  const [name, setName] = useState("Thinh");
+  const [age, setAge] = useState("20");
+  const handleOnChangeInput = (event) => {
+    setName(event.target.value);
   };
-  handleOnChangeAge = (event) => {
-    this.setState({
-      age: event.target.value,
-    });
-    // console.log(event, event.target.value);
+  const handleOnChangeAge = (event) => {
+    setAge(event.target.value);
   };
-  handleOnSubmit = (event) => {
+  const handleOnSubmit = (event) => {
     event.preventDefault();
-    this.props.handleAddNewUser({
+    props.handleAddNewUser({
       id: Math.floor(Math.random() * 100 + 1) + "-random",
-      name: this.state.name,
-      age: this.state.age,
+      name: name,
+      age: age,
     });
   };
-  render() {
-    return (
-      <div>
-        My name is {this.state.name} I'm from {this.state.age}
-        <form onSubmit={(event) => this.handleOnSubmit(event)}>
-          <label>Your name:</label>
-          <input
-            type="text"
-            value={this.state.name}
-            onChange={(event) => this.handleOnChangeInput(event)}
-          />
-          <label>Your age:</label>
-          <input
-            type="text"
-            value={this.state.age}
-            onChange={(event) => this.handleOnChangeAge(event)}
-          />
-          <button>Submit</button>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      My name is {name} I'm from {age}
+      <form onSubmit={(event) => handleOnSubmit(event)}>
+        <label>Your name:</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(event) => handleOnChangeInput(event)}
+        />
+        <label>Your age:</label>
+        <input
+          type="text"
+          value={age}
+          onChange={(event) => handleOnChangeAge(event)}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default AddUserInfor;
--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./DisplayInfor.scss";
-import logo from "../logo.svg";
+// import logo from "../logo.svg";
 
 //stateless component vs stateful component
 // class DisplayInfor extends React.Component {
@@ -47,9 +47,28 @@ import logo from "../logo.svg";
 
 const DisplayInfor = (props) => {
   const { listUser } = props; //object
+  const [isShowHideListUsers, setShowHideListUsers] = useState(true); // [true, function
+  //destructuring assignment
+  const handleShowHideListUsers = () => {
+    setShowHideListUsers(!isShowHideListUsers);
+  };
+
+  console.log("call me");
+  useEffect(() => {
+    setTimeout(() => {
+      document.title = "Ivor's website";
+    }, 3000);
+    console.log("call me when component did mount");
+  }, []); //[] là cho useEffect chạy 1 lần duy nhất khi component được render
+  //[listUser] là cho useEffect chạy mỗi khi listUser thay đổi
   return (
     <div className="display-infor-container">
-      {true && (
+      <div>
+        <span onClick={() => handleShowHideListUsers()}>
+          {isShowHideListUsers === true ? "Hide list user" : "Show list user"}
+        </span>
+      </div>
+      {isShowHideListUsers && (
         <>
           {listUser.map((user, index) => {
             return (
